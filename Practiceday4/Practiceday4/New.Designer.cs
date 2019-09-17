4c38bb6e19917eef896a4cee90310d5ba27568e3
munnafdewan <engr.munnaf@gmail.com>
2019-09-17 20:25:36 +0600

    Practice4

---
+++ Practiceday4/Practiceday4/New.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Practiceday4
+{
+    partial class New
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.namee = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // namee
+            // 
+            this.namee.AutoSize = true;
+            this.namee.Location = new System.Drawing.Point(45, 46);
+            this.namee.Name = "namee";
+            this.namee.Size = new System.Drawing.Size(35, 13);
+            this.namee.TabIndex = 0;
+            this.namee.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(105, 46);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(141, 147);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showRichTextBox
+            // 
+            this.showRichTextBox.Location = new System.Drawing.Point(328, 31);
+            this.showRichTextBox.Name = "showRichTextBox";
+            this.showRichTextBox.Size = new System.Drawing.Size(368, 281);
+            this.showRichTextBox.TabIndex = 3;
+            this.showRichTextBox.Text = "";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(48, 147);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 4;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(105, 89);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ageTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Age";
+            // 
+            // New
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.showRichTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.namee);
+            this.Name = "New";
+            this.Text = "New";
+            this.Load += new System.EventHandler(this.New_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label namee;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.RichTextBox showRichTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Practiceday4/Practiceday4/New.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practiceday4
+{
+    public partial class New : Form
+    {
+        List<string> names = new List<string> {"Munnaf"};
+        List<int> ages = new List<int> { 15 };
+        public New()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            names.Add(nameTextBox.Text);
+            ages.Add(Convert.ToInt32(ageTextBox.Text));
+            ShowCustomer();
+            MessageBox.Show("Add");
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            ShowCustomer();
+        }
+
+        private void ShowCustomer()
+        {
+
+            string message = " ";
+
+            for (int i = 0; i < names.Count(); i++)
+            {
+                message += "Name : " + names[i] + "Age :" + ages[i] + "\n";
+
+            }
+            showRichTextBox.Text = message;
+            //string message = "";
+
+            //foreach (string name in names)
+            //{
+            //    message += "Name : " + name + "\n";
+            //}
+            //showRichTextBox.Text = message;
+        }
+
+        private void New_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
